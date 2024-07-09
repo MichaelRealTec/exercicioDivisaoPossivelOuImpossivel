37c5b9ca49ab487e9f31732e2f787ad4236da561
Michael Rafael de Andrade <realteceinformatica@gmail.com>
2024-07-09 01:59:40 -0300

    exercicio resolvido - divisaoPossivelOuImpossivel

--- src/entities/Program.java
+++ src/entities/Program.java
@@ -1,11 +1,36 @@
 package entities;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// divisaoPossivelOuImpossivel
-		
+		int n;
+		Double numerador;
+		Double denominador;
+		Double divisao;
 
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Quantos casos você vai digitar? ");
+		n = sc.nextInt();
+
+		for (int i = 1; i <= n; i++) {
+			System.out.print("Entre com o numerador: ");
+			numerador = sc.nextDouble();
+			System.out.print("Entre com o denominador: ");
+			denominador = sc.nextDouble();
+			if (denominador == 0) {
+				System.out.println("DIVISAO IMPOSSIVEL");
+			} else {
+				divisao = numerador / denominador;
+				System.out.printf("DIVISAO = %.2f%n", divisao);
+			}
+		}
+		sc.close();
 	}
 
 }
